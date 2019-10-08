4fcd5e688a97b0e9a8b7ecc66b8f89e323c02958
Shelb689 <43571202+Shelb689@users.noreply.github.com>
2019-10-08 15:15:16 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    std::cout << "Welcome to my program." << std::endl;
+
+      int a = 7;
+      int b = 3;
+
+      std::cout << "Please enter a number: ";
+      std::cin >> a;
+
+
+     std::cout << "Please enter a 2nd number: ";
+     std::cin >> b;
+
+     std::cout << ( a +  b) << std::endl;
+
+
+      return 0;
+}
